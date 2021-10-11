f68bf6eddc2a54a3a06efe41740bc429b82787df
Marcos <marcosdp12@gmail.com>
2021-10-11 11:57:40 -0300

    configure to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import dotenv from 'dotenv'
 import express from 'express'
 import connectDB from './config/db.js'
@@ -16,7 +17,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/exercises', exerciseRouter);
-app.use('/users', userRouter);
+app.use('/users', userRouter)
+
+const __dirname = path.resolve()
+
+if(process.env.NODE_ENV !== 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} 
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, ()=>{
